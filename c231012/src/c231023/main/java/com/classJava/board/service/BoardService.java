56d2e28ae1a5d3ba8342a55d2f81cb2b037688d0
Jeong Kyung Hoon <35556229+KyungHoonJ@users.noreply.github.com>
2023-10-23 11:04:19 +0900

    feat: add board in spring mvc structure

---
+++ c231012/src/c231023/main/java/com/classJava/board/service/BoardService.java
@@ -0,0 +1,49 @@
+package c231023.main.java.com.classJava.board.service;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import c231023.main.java.com.classJava.board.dao.BoardDAO;
+import c231023.main.java.com.classJava.board.domain.Board;
+import c231023.main.java.com.classJava.user.dao.UserDAO;
+import c231023.main.java.com.classJava.user.domain.User;
+
+public class BoardService {
+	private UserDAO userDAO;
+	private BoardDAO boardDAO;
+
+	public void setUserDAO(UserDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+
+	public void setBoardDAO(BoardDAO boardDAO) {
+		this.boardDAO = boardDAO;
+	}
+
+	public void add(Board board, User user) {
+		board.setUserId(user.getId());
+		boardDAO.add(board);
+	}
+
+	public Board get(int id) {
+		Board board = boardDAO.get(id);
+		User user = userDAO.get(board.getUserId());
+		board.setUser(user);
+
+		return board;
+	}
+	
+	public List<Board> getAll(){
+		List<Board> list = boardDAO.getAll();
+		for(int i = 0; i < list.size();++i) {
+			int userId = list.get(i).getUserId();
+			User user = userDAO.get(userId);
+			list.get(i).setUser(user);
+		}
+		return list;
+	}
+
+}
