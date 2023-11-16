43f5eccc110a6fd1a077ee1372790d559df404ed
Jeong Kyung Hoon <35556229+KyungHoonJ@users.noreply.github.com>
2023-11-16 12:07:19 +0900

    feat: add thymeleaf

--- jdbc-test/src/main/java/com/kyungiljava4/jdbctest/board/controller/BoardController.java
+++ jdbc-test/src/main/java/com/kyungiljava4/jdbctest/board/controller/BoardController.java
@@ -5,8 +5,10 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -23,9 +25,23 @@ public class BoardController {
 		return "boards/index";
 	}
 
+	@GetMapping("/board/thyme")
+	public String listThymePage(Model model) {
+		List<Board> list = boardService.getAll();
+		model.addAttribute("list", list);
+		model.addAttribute("test", "이거 읽어올 수 있나?");
+		model.addAttribute("tag", "<strong>이거 읽어올 수 있나?</strong>");
+		return "boards/thyme/index";
+	}
+
 	@GetMapping("/board/add")
 	public String addPage() {
 		return "boards/add";
+	}
+
+	@GetMapping("/board/item")
+	public String itemPage() {
+		return "boards/item";
 	}
 
 	@ResponseBody
@@ -39,7 +55,8 @@ public class BoardController {
 			sb.append("\"user\":\"" + list.get(i).getUser() + "\",");
 			sb.append("\"title\":\"" + list.get(i).getTitle() + "\",");
 			sb.append("\"content\":\"" + list.get(i).getContent() + "\"}");
-			if(i < list.size() - 1) sb.append(",");
+			if (i < list.size() - 1)
+				sb.append(",");
 		}
 		sb.append("]");
 		return sb.toString();
@@ -50,5 +67,19 @@ public class BoardController {
 		boardService.add(new Board(data.get("user"), data.get("title"), data.get("content")));
 
 		return "redirect:/board";
+	}
+
+	@PostMapping("/board/item")
+	@ResponseBody
+	public String get(@RequestBody Map<String, String> data) {
+		Board board = boardService.get(Integer.parseInt(data.get("id")));
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("{\"id\":" + board.getId() + ",");
+		sb.append("\"user\":\"" + board.getUser() + "\",");
+		sb.append("\"title\":\"" + board.getTitle() + "\",");
+		sb.append("\"content\":\"" + board.getContent() + "\"}");
+
+		return sb.toString();
 	}
 }
