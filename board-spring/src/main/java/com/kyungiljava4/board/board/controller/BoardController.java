9e733190c2c67c6ccb27015f39843392643bda93
Jeong Kyung Hoon <35556229+KyungHoonJ@users.noreply.github.com>
2023-11-20 11:17:49 +0900

    feat: edit board page

--- board-spring/src/main/java/com/kyungiljava4/board/board/controller/BoardController.java
+++ board-spring/src/main/java/com/kyungiljava4/board/board/controller/BoardController.java
@@ -1,19 +1,39 @@
 package com.kyungiljava4.board.board.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.kyungiljava4.board.board.domain.Board;
+import com.kyungiljava4.board.board.service.BoardService;
 
 @Controller
 public class BoardController {
+	@Autowired
+	BoardService boardService;
+
 	@GetMapping("/")
 	public String boardMainPage(Model model) {
 		model.addAttribute("title", "게시판");
 		model.addAttribute("path", "/board/index");
 		model.addAttribute("content", "boardFragment");
 		model.addAttribute("contentHead", "boardFragmentHead");
+		model.addAttribute("list", boardService.getAll());
 		return "/basic/layout";
 	}
+
+	@PostMapping("/add")
+	public String add(@RequestParam Map<String, String> data) {
+		boardService.add(new Board(data.get("title"), data.get("content"), 1));
+		
+		return "redirect:/";
+	}
+
 	@GetMapping("/notice")
 	public String noticePage(Model model) {
 		model.addAttribute("title", "공지사항");
