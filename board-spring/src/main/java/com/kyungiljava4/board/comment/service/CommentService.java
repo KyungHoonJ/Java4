b3c18ad804a191bc2cb3032267d436bcb08be349
Jeong Kyung Hoon <35556229+KyungHoonJ@users.noreply.github.com>
2023-11-27 13:51:59 +0900

    feat: add comments

---
+++ board-spring/src/main/java/com/kyungiljava4/board/comment/service/CommentService.java
@@ -0,0 +1,43 @@
+package com.kyungiljava4.board.comment.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kyungiljava4.board.comment.dao.CommentDao;
+import com.kyungiljava4.board.comment.domain.Comment;
+
+@Service
+public class CommentService {
+	@Autowired
+	CommentDao commentDao;
+
+	public void add(Comment comment) {
+		commentDao.add(comment);
+	}
+
+	public List<Comment> getComments(int boardId, int start) {
+		List<Comment> list = commentDao.getParents(boardId, start);
+//		for (int i = 0; i < list.size(); ++i) {
+//			list.get(i).setChildren(
+//					commentDao.getChildren(
+//							boardId, 
+//							list.get(i).getId()
+//							)
+//					);
+//		}
+		list.forEach((item)->{
+			item.setChildren(getChildren(boardId, item));
+		});
+		return list;
+	}
+	
+	private List<Comment> getChildren(int boardId, Comment comment){
+		List<Comment> list = commentDao.getChildren(boardId, comment.getId());
+		list.forEach((item)->{
+			item.setChildren(getChildren(boardId, item));
+		});
+		return list;
+	}
+}
